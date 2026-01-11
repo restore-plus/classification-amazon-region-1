f9bb24e2d4d66008d660e932ff0261482b44199c
Felipe Carlos <efelipecarlos@gmail.com>
2026-01-11 10:08:47 -0300

    prodes: update base prodes generation

--- analysis/masks/mask-base-prodes.R
+++ analysis/masks/mask-base-prodes.R
@@ -2,44 +2,25 @@ library(sits)
 library(restoreutils)
 
 #
-# Config: Connection timeout
-#
-options(timeout = max(720, getOption("timeout")))
-
-
-#
 # General definitions
 #
-memsize    <- 140
-multicores <- 44
+memsize    <- 100
+multicores <- 35
 
 version <- "v2"
 
-mask_years <- c(2002, 2003)
-
+year_to_prepare <- 2024:2008
 
 #
-# 1) Download Prodes data
+# 1 Generate forest mask
 #
-restoreutils::prepare_prodes(
-  region_id = 1,
-  years     = c(2002, 2003)
+prepare_prodes(
+  region_id          = 1,
+  years              = year_to_prepare,
+  version            = version,
+  multicores         = multicores,
+  memsize            = memsize,
+  nonforest_mask     = TRUE,
+  nonforest_complete = TRUE,
+  fix_pre_aggregation_prodes = TRUE
 )
-
-
-#
-# 2) Generate forest mask
-#
-# Note: We start generating masks in 2023, as 2024 is the most recent data, and
-#       all forest there is the current forest. So, there is no requirements for
-#       extra data transformations
-#
-purrr::map(mask_years, function(mask_year) {
-  restoreutils::prodes_generate_mask(
-    target_year    = mask_year,
-    version        = version,
-    multicores     = multicores,
-    memsize        = memsize,
-    nonforest_mask = TRUE
-  )
-})
